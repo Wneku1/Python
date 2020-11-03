aada49c3e4b39bfeb8954f7d8ef36ff76eb3f59b
Sebastian W <sebastianwnek712@gmail.com>
2020-11-03 22:53:23 +0100

    Remover słów

---
+++ Text/removeWord.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+wordToRemove: Tuple[str, str, str, str] = ("się", "i", "oraz", "nigdy")
+
+temporaryText = []
+
+file = input("Give me file: ")
+
+with open(file, 'r', encoding='utf8') as fin:
+    for line in fin:
+        for word in wordToRemove:
+            line = line.replace(("{} ".format(word)), "")
+            line = line.replace((" {} ".format(word)), "")
+            line = line.replace(" {}".format(word), "")
+        temporaryText.append(line)
+
+
+with open(file, 'w+') as fout:
+    for line in temporaryText:
+        fout.write(line)
