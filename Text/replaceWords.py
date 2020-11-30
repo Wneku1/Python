5f8dcf31c7bb318b63d0018cd25c6a7f54af2fe2
Sebastian W <sebastianwnek712@gmail.com>
2020-11-30 16:55:01 +0100

    Dodane zamienianie słów

---
+++ Text/replaceWords.py
@@ -0,0 +1,21 @@
+from typing import List
+import re
+
+temporaryText: List[str] = []
+tmpLine: List[str] = []
+
+file = input("Give me file: ")
+
+temp = "###ABABA###"
+with open(file, 'r', encoding='utf8') as fin:
+    for line in fin:
+        line = re.sub(r'\bi\b', temp, line)
+        line = re.sub(r'\boraz\b', 'i', line)
+        line = re.sub(temp, 'oraz', line)
+        line = re.sub(r'\bnigdy\b', 'prawie nigdy', line)
+        line = re.sub(r'\bdlaczego\b', 'czemu', line)
+        temporaryText.append(line)
+
+with open(file, 'w+') as fout:
+    for line in temporaryText:
+        fout.write(line)
